b9d5f4b000f1ea6c775805dce2377877ee5fab1b
EF <eorf256@gmail.com>
2021-10-26 02:22:25 +0900

    InGameに入るとフェードインするように変更。

--- Assets/Game/Scripts/Scene/InGame.cs
+++ Assets/Game/Scripts/Scene/InGame.cs
@@ -1,7 +1,20 @@
+using System;
 using UnityEngine;
 
 public sealed class InGame : SceneBase
 {
+    private IScreenFader fader;
+    
+    private void Awake()
+    {
+        fader = FindObjectOfType<ScreenFaderBase>();
+    }
+
+    private void OnEnable()
+    {
+        fader.FadeIn();
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.X))
