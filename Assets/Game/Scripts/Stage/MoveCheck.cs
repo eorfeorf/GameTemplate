695d444517e1f521c2d726bc6ecd5605109a8706
EF <eorf256@gmail.com>
2021-10-30 01:30:07 +0900

    いろいろ追加

---
+++ Assets/Game/Scripts/Stage/MoveCheck.cs
@@ -0,0 +1,11 @@
+
+public class MoveCheck
+{
+    private Stage stage;
+    
+    public bool Check()
+    {
+        // ステージの状態を見て移動できるかチェック.
+        return true;
+    }
+}
---
+++ Assets/Game/Scripts/Stage/Stage.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+/// <summary>
+/// ステージの管理
+/// ステージの生成、ステージの破棄
+/// </summary>
+public class Stage : MonoBehaviour
+{
+    
+
+    private void Start()
+    {
+        // プレイヤーの位置を見る
+    }
+
+    public void Make()
+    {
+        
+    }
+
+    public void Destroy()
+    {
+        
+    }
+}
