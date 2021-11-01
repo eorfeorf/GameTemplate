b0539acc4e908206be299c55416503584b91f670
EF <eorf256@gmail.com>
2021-11-02 00:06:12 +0900

    修正

--- Assets/Game/Scripts/Input/PlayerInputProvider.cs
+++ Assets/Game/Scripts/Input/PlayerInputProvider.cs
@@ -7,17 +7,17 @@ namespace Game.Scripts.Player
     public class PlayerInputProvider : MonoBehaviour, IInputProvider
     {
         public IReadOnlyReactiveProperty<Unit> OnPushedLeft => onPushedLeft;
-        private ReactiveProperty<Unit> onPushedLeft;
+        private ReactiveProperty<Unit> onPushedLeft = new ReactiveProperty<Unit>();
         public IReadOnlyReactiveProperty<Unit> OnPushedRight => onPushedRight;
-        private ReactiveProperty<Unit> onPushedRight;
+        private ReactiveProperty<Unit> onPushedRight = new ReactiveProperty<Unit>();
         public IReadOnlyReactiveProperty<Unit> OnPushedUp => onPushedUp;
-        private ReactiveProperty<Unit> onPushedUp;
+        private ReactiveProperty<Unit> onPushedUp = new ReactiveProperty<Unit>();
         public IReadOnlyReactiveProperty<Unit> OnPushedDown => onPushedDown;
-        private ReactiveProperty<Unit> onPushedDown;
+        private ReactiveProperty<Unit> onPushedDown = new ReactiveProperty<Unit>();
         public IReadOnlyReactiveProperty<Unit> OnPushedDecide => onPushedDecide;
-        private ReactiveProperty<Unit> onPushedDecide;
+        private ReactiveProperty<Unit> onPushedDecide = new ReactiveProperty<Unit>();
         public IReadOnlyReactiveProperty<Unit> OnPushedCancel => onPushedCancel;
-        private ReactiveProperty<Unit> onPushedCancel;
+        private ReactiveProperty<Unit> onPushedCancel = new ReactiveProperty<Unit>();
 
         private void Update()
         {
