8563bf6e148547a4848693de5bbd04cbf8b10db5
EF <eorf256@gmail.com>
2021-11-20 11:50:29 +0900

    入力のインターフェイスを変更

--- Assets/GameFramework/Input/IInputEvent.cs
+++ Assets/GameFramework/Input/IInputEvent.cs
@@ -2,8 +2,11 @@ using UniRx;
 
 public interface IInputEventProvider
 {
-    IReadOnlyReactiveProperty<bool> MainButton { get; }
-    IReadOnlyReactiveProperty<bool> SubButton { get; }
-    IReadOnlyReactiveProperty<float> Vertical { get; }
-    IReadOnlyReactiveProperty<float> Horizontal { get; }
+    public IReadOnlyReactiveProperty<float> OnVertical { get; }
+    public IReadOnlyReactiveProperty<float> OnHorizontal { get; }
+    public IReadOnlyReactiveProperty<Unit> OnPushedLeft { get; }
+    public IReadOnlyReactiveProperty<Unit> OnPushedRight { get; }
+    public IReadOnlyReactiveProperty<Unit> OnPushedUp { get; }
+    public IReadOnlyReactiveProperty<Unit> OnPushedDown { get; }
+    public IReadOnlyReactiveProperty<Unit> OnPushedDecide { get; }
 }
