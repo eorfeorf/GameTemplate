b6bf654067ad1afb7c042b067916102c4e2a78ba
EF <eorf256@gmail.com>
2021-11-22 23:05:42 +0900

    タッチのインターフェイスを追加

--- Assets/GameFramework/Input/IInputEventProvider.cs
+++ Assets/GameFramework/Input/IInputEventProvider.cs
@@ -1,18 +1,6 @@
 using UniRx;
 using UnityEngine;
 
-public class TouchInfo
-{
-    public bool Active;
-    public Touch Touch;
-
-    public TouchInfo(bool active, Touch touch)
-    {
-        Active = active;
-        Touch = touch;
-    }
-}
-
 public interface IInputEventProvider
 {
     public IReadOnlyReactiveProperty<float> OnVertical { get; }
@@ -22,15 +10,6 @@ public interface IInputEventProvider
     public IReadOnlyReactiveProperty<Unit> OnPushedUp { get; }
     public IReadOnlyReactiveProperty<Unit> OnPushedDown { get; }
     public IReadOnlyReactiveProperty<Unit> OnPushedDecide { get; }
-    
-    public IReadOnlyReactiveProperty<TouchInfo> OnTouched0 { get; }
-    public IReadOnlyReactiveProperty<TouchInfo> OnTouched1 { get; }
-    public IReadOnlyReactiveProperty<TouchInfo> OnTouched2 { get; }
-    public IReadOnlyReactiveProperty<TouchInfo> OnTouched3 { get; }
-    public IReadOnlyReactiveProperty<TouchInfo> OnTouched4 { get; }
-    public IReadOnlyReactiveProperty<TouchInfo> OnTouched5 { get; }
-    public IReadOnlyReactiveProperty<TouchInfo> OnTouched6 { get; }
-    public IReadOnlyReactiveProperty<TouchInfo> OnTouched7 { get; }
-    public IReadOnlyReactiveProperty<TouchInfo> OnTouched8 { get; }
-    public IReadOnlyReactiveProperty<TouchInfo> OnTouched9 { get; }
+
+    public IReadOnlyReactiveProperty<Touch[]> OnTouches { get; }
 }
